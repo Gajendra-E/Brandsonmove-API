a19a3c1d2a751217aa581023ce64d98b00cd7b87
Gajendra-Eggadi <gajendracse521@gmail.com>
2023-12-25 07:13:30 +0530

    contactinfo updated

--- src/api/routes/contactinfo.js
+++ src/api/routes/contactinfo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var contactInfoController =require('../../controllers/contactInfo') 
  import checkAuth from "../../middleware/check-auth";
-
+// API END POINTS
 router.get('/',  contactInfoController.fetch_all_contact_infos)
 router.post('/',  contactInfoController.create_contact_info)
 router.put('/:id', contactInfoController.update_contact_info)
